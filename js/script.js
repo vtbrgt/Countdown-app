ea5ae636dac3e796062f24675cf6d1563cb2c7f6
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-29 23:48:11 -0300

    adding error message

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 import Countdown from './countdown.js';
 
-/* setInterval(() => {
-  console.log(tempoParaOAnoNovo.total);
-}, 1000); */
-
-// + '00:00:00 GMT-0300'
-
 const dateInput = document.querySelector('#data');
+const contador = document.querySelector('.contador');
+const erro = document.querySelector('.erro');
 const days = document.querySelector('.days');
 const hours = document.querySelector('.hours');
 const minutes = document.querySelector('.minutes');
@@ -14,26 +10,38 @@ const seconds = document.querySelector('.seconds');
 
 let timeLeft;
 
-dateInput.addEventListener('change', () => {
+dateInput.addEventListener('change', initCountdown);
+
+function initCountdown() {
   const dateChosen = new Date(dateInput.value);
 
   timeLeft = new Countdown(dateChosen);
   if (timeLeft.total.hours > 1) {
+    contador.classList.add('ativo');
     days.innerText = timeLeft.total.days;
     hours.innerText = timeLeft.total.hours;
     minutes.innerText = timeLeft.total.minutes;
     seconds.innerText = timeLeft.total.seconds;
+    erro.style.display = 'none';
+  } else {
+    contador.classList.remove('ativo');
+    erro.style.display = 'block';
   }
-});
 
-setInterval(() => {
-  const dateChosen = new Date(dateInput.value);
+  setInterval(() => {
+    const dateChosen = new Date(dateInput.value);
 
-  timeLeft = new Countdown(dateChosen);
-  if (timeLeft.total.hours > 1) {
-    days.innerText = timeLeft.total.days;
-    hours.innerText = timeLeft.total.hours;
-    minutes.innerText = timeLeft.total.minutes;
-    seconds.innerText = timeLeft.total.seconds;
-  }
-}, 1000);
+    timeLeft = new Countdown(dateChosen);
+    if (timeLeft.total.hours > 1) {
+      contador.classList.add('ativo');
+      days.innerText = timeLeft.total.days;
+      hours.innerText = timeLeft.total.hours;
+      minutes.innerText = timeLeft.total.minutes;
+      seconds.innerText = timeLeft.total.seconds;
+      erro.style.display = 'none';
+    } else {
+      contador.classList.remove('ativo');
+      erro.style.display = 'block';
+    }
+  }, 1000);
+}
