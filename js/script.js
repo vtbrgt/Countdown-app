a807b1d09c438543cb4c36a00a3ebe347e34a716
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-29 23:27:34 -0300

    countdown is now function

--- js/script.js
+++ js/script.js
@@ -7,11 +7,33 @@ import Countdown from './countdown.js';
 // + '00:00:00 GMT-0300'
 
 const dateInput = document.querySelector('#data');
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+let timeLeft;
 
 dateInput.addEventListener('change', () => {
   const dateChosen = new Date(dateInput.value);
-  console.log(dateChosen);
 
-  var timeLeft = new Countdown(dateChosen);
-  console.log(timeLeft);
+  timeLeft = new Countdown(dateChosen);
+  if (timeLeft.total.hours > 1) {
+    days.innerText = timeLeft.total.days;
+    hours.innerText = timeLeft.total.hours;
+    minutes.innerText = timeLeft.total.minutes;
+    seconds.innerText = timeLeft.total.seconds;
+  }
 });
+
+setInterval(() => {
+  const dateChosen = new Date(dateInput.value);
+
+  timeLeft = new Countdown(dateChosen);
+  if (timeLeft.total.hours > 1) {
+    days.innerText = timeLeft.total.days;
+    hours.innerText = timeLeft.total.hours;
+    minutes.innerText = timeLeft.total.minutes;
+    seconds.innerText = timeLeft.total.seconds;
+  }
+}, 1000);
